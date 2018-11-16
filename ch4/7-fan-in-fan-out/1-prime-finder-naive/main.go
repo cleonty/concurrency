d00ca889477e939162fea657c6ba87d4837a5f0e
Leonty Chudinov <leonty@inbox.ru>
2018-11-16 20:51:09 +0500

    prime finder naive

---
+++ ch4/7-fan-in-fan-out/1-prime-finder-naive/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func main() {
+	rand := func() interface{} { return rand.Intn(50000000) }
+	done := make(chan interface{})
+	defer close(done)
+
+	start := time.Now()
+	randIntStream := toInt(done, repeatFn(done, rand))
+	fmt.Println("Primes:")
+	for prime := range take(done, primeFinder(done, randIntStream), 10) {
+		fmt.Printf("\t%d\n", prime)
+	}
+	fmt.Printf("Search took: %v", time.Since(start))
+}
