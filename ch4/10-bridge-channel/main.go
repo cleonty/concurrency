f32ddcb7fb4eb73c8eb9b685c17169b98e0a4aa2
Leonty Chudinov <leonty@inbox.ru>
2018-11-19 08:18:49 +0500

    bridge channel

---
+++ ch4/10-bridge-channel/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func genVals() <-chan <-chan interface{} {
+	chanStream := make(chan (<-chan interface{}))
+	go func() {
+		defer close(chanStream)
+		for i := 0; i < 10; i++ {
+			stream := make(chan interface{}, 1)
+			stream <- i
+			close(stream)
+			chanStream <- stream
+		}
+	}()
+	return chanStream
+}
+
+func main() {
+	for v := range bridge(nil, genVals()) {
+		fmt.Printf("%v ", v)
+	}
+}
